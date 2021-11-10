c58b1925dac234c122936c399d38758ee8f7f1fe
arsong011 <91659464+arsong011@users.noreply.github.com>
2021-11-10 14:16:36 +0900

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,18 @@
 library(data.table)
 library(dplyr)
 
-featureNames <- read.table("C:/Users/82106/Documents/서울대병원/참고자료/coursera/UCI HAR Dataset/features.txt")
-activityLabels <- read.table("C:/Users/82106/Documents/서울대병원/참고자료/coursera/UCI HAR Dataset/activity_labels.txt", header = FALSE)
+featureNames <- read.table("/UCI HAR Dataset/features.txt")
+activityLabels <- read.table("/UCI HAR Dataset/activity_labels.txt", header = FALSE)
 
 #Format training and test data sets
 
-subjectTrain <- read.table("C:/Users/82106/Documents/서울대병원/참고자료/coursera/UCI HAR Dataset/train/subject_train.txt", header = FALSE)
-activityTrain <- read.table("C:/Users/82106/Documents/서울대병원/참고자료/coursera/UCI HAR Dataset/train/y_train.txt", header = FALSE)
-featuresTrain <- read.table("C:/Users/82106/Documents/서울대병원/참고자료/coursera/UCI HAR Dataset/train/X_train.txt", header = FALSE)
+subjectTrain <- read.table("/UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+activityTrain <- read.table("/UCI HAR Dataset/train/y_train.txt", header = FALSE)
+featuresTrain <- read.table("/UCI HAR Dataset/train/X_train.txt", header = FALSE)
 
-subjectTest <- read.table("C:/Users/82106/Documents/서울대병원/참고자료/coursera/UCI HAR Dataset/test/subject_test.txt", header = FALSE)
-activityTest <- read.table("C:/Users/82106/Documents/서울대병원/참고자료/coursera/UCI HAR Dataset/test/y_test.txt", header = FALSE)
-featuresTest <- read.table("C:/Users/82106/Documents/서울대병원/참고자료/coursera/UCI HAR Dataset/test/X_test.txt", header = FALSE)
+subjectTest <- read.table("/UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+activityTest <- read.table("/UCI HAR Dataset/test/y_test.txt", header = FALSE)
+featuresTest <- read.table("/UCI HAR Dataset/test/X_test.txt", header = FALSE)
 
 
 #1. Merge the training and the test sets to create one data set
